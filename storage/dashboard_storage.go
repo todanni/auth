f657d89613388998f5b84b2b1b272e169b0f231d
Danni Popova <dannii.popova@gmail.com>
2022-09-14 17:27:26 +0100

    Convert where needed

--- storage/dashboard_storage.go
+++ storage/dashboard_storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"errors"
 
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 
 	"github.com/todanni/auth/models"
@@ -40,7 +41,7 @@ func (s dashboardStorage) List(userid uint) ([]models.Dashboard, error) {
 func (s dashboardStorage) Create(owner, invited uint) (models.Dashboard, error) {
 	dashboard := models.Dashboard{
 		Owner:   owner,
-		Members: []uint{owner, invited},
+		Members: pq.Int64Array{int64(owner), int64(invited)},
 		Status:  models.PendingStatus,
 	}
 
