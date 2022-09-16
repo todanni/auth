ddf17bcbe778d791b344576bd409783efab8e170
Danni Popova <dannii.popova@gmail.com>
2022-09-16 21:03:32 +0100

    Add trailing slash to api path

--- service/dashboard/routes.go
+++ service/dashboard/routes.go
@@ -4,8 +4,8 @@ import "net/http"
 
 func (s *dashboardService) routes() {
 	// GET an dashboard
-	s.router.HandleFunc("/api/dashboards", s.CreateDashboardHandler).Methods(http.MethodPost)
-	s.router.HandleFunc("/api/dashboards", s.ListDashboardsHandler).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/dashboards/", s.CreateDashboardHandler).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/dashboards/", s.ListDashboardsHandler).Methods(http.MethodGet)
 	s.router.HandleFunc("/api/dashboards/{id}", s.DeleteDashboardHandler).Methods(http.MethodDelete)
 	s.router.HandleFunc("/api/dashboards/{id}", s.GetDashboardHandler).Methods(http.MethodGet)
 	s.router.HandleFunc("/api/dashboards/{id}/accept", s.AcceptDashboardInviteHandler).Methods(http.MethodPut)
