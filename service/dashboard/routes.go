a445af52f7505eef9948b1f58193d2194847a933
Oliver Roberts <3415597+stegzilla@users.noreply.github.com>
2022-09-20 17:29:52 +0100

    fix paths

--- service/dashboard/routes.go
+++ service/dashboard/routes.go
@@ -7,8 +7,8 @@ import (
 )
 
 func (s *dashboardService) routes() {
-	s.router.Handle("/api/dashboards", middleware.NewEnsureAuth(s.CreateDashboardHandler)).Methods(http.MethodPost)
-	s.router.Handle("/api/dashboards", middleware.NewEnsureAuth(s.ListDashboardsHandler)).Methods(http.MethodGet)
+	s.router.Handle("/api/dashboards/", middleware.NewEnsureAuth(s.CreateDashboardHandler)).Methods(http.MethodPost)
+	s.router.Handle("/api/dashboards/", middleware.NewEnsureAuth(s.ListDashboardsHandler)).Methods(http.MethodGet)
 	s.router.Handle("/api/dashboards/{id}", middleware.NewEnsureAuth(s.DeleteDashboardHandler)).Methods(http.MethodDelete)
 	s.router.Handle("/api/dashboards/{id}", middleware.NewEnsureAuth(s.GetDashboardHandler)).Methods(http.MethodGet)
 	s.router.Handle("/api/dashboards/{id}/accept", middleware.NewEnsureAuth(s.AcceptDashboardInviteHandler)).Methods(http.MethodPut)
