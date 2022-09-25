e1a768cfbe30390601badce0cccd7445e2035ee6
Danni Popova <dannii.popova@gmail.com>
2022-09-25 17:31:44 +0100

    Attempt to test

--- service/dashboard/routes.go
+++ service/dashboard/routes.go
@@ -6,11 +6,18 @@ import (
 	"github.com/todanni/token"
 )
 
+const (
+	ListAndCreateDashboardHandler = "/api/dashboards/"
+	GetAndDeleteDashboardHandler  = "/api/dashboards/{id}"
+	AcceptDashboardHandler        = "/api/dashboards/{id}/accept"
+	RejectDashboardHandler        = "/api/dashboards/{id}/reject"
+)
+
 func (s *dashboardService) routes() {
-	s.router.Handle("/api/dashboards/", token.NewAuthenticationMiddleware(s.CreateDashboardHandler)).Methods(http.MethodPost)
-	s.router.Handle("/api/dashboards/", token.NewAuthenticationMiddleware(s.ListDashboardsHandler)).Methods(http.MethodGet)
-	s.router.Handle("/api/dashboards/{id}", token.NewAuthenticationMiddleware(s.DeleteDashboardHandler)).Methods(http.MethodDelete)
-	s.router.Handle("/api/dashboards/{id}", token.NewAuthenticationMiddleware(s.GetDashboardHandler)).Methods(http.MethodGet)
-	s.router.Handle("/api/dashboards/{id}/accept", token.NewAuthenticationMiddleware(s.AcceptDashboardInviteHandler)).Methods(http.MethodPut)
-	s.router.Handle("/api/dashboards/{id}/reject", token.NewAuthenticationMiddleware(s.RejectDashboardInviteHandler)).Methods(http.MethodPut)
+	s.router.Handle(ListAndCreateDashboardHandler, token.NewAuthenticationMiddleware(s.CreateDashboardHandler)).Methods(http.MethodPost)
+	s.router.Handle(ListAndCreateDashboardHandler, token.NewAuthenticationMiddleware(s.ListDashboardsHandler)).Methods(http.MethodGet)
+	s.router.Handle(GetAndDeleteDashboardHandler, token.NewAuthenticationMiddleware(s.DeleteDashboardHandler)).Methods(http.MethodDelete)
+	s.router.Handle(GetAndDeleteDashboardHandler, token.NewAuthenticationMiddleware(s.GetDashboardHandler)).Methods(http.MethodGet)
+	s.router.Handle(AcceptDashboardHandler, token.NewAuthenticationMiddleware(s.AcceptDashboardInviteHandler)).Methods(http.MethodPut)
+	s.router.Handle(RejectDashboardHandler, token.NewAuthenticationMiddleware(s.RejectDashboardInviteHandler)).Methods(http.MethodPut)
 }
--- service/dashboard/server_test.go
+++ service/dashboard/server_test.go
@@ -1,1 +1,61 @@
 package dashboard
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/todanni/token"
+	"gorm.io/gorm"
+
+	"github.com/todanni/auth/models"
+	"github.com/todanni/auth/storage/mocks"
+)
+
+func Test_dashboardService_ListDashboardsHandler(t *testing.T) {
+	r := mux.NewRouter()
+
+	dashboardStorageMock := mocks.NewDashboardStorage(t)
+	dashboardStorageMock.On("List", mock.Anything).Return([]models.Dashboard{{
+		Owner:  1,
+		Status: models.PendingStatus,
+		Members: []models.User{{
+			Model: gorm.Model{
+				ID: 1,
+			},
+			Email:      "test1@mail.com",
+			ProfilePic: "profile-pic1.jpeg",
+		},
+			{
+				Model: gorm.Model{
+					ID: 2,
+				},
+				Email:      "test2@mail.com",
+				ProfilePic: "profile-pic2.jpeg",
+			}},
+	}}, nil)
+
+	userStorageMock := mocks.NewUserStorage(t)
+
+	s := NewDashboardService(dashboardStorageMock, userStorageMock, r)
+
+	w := httptest.NewRecorder()
+	r.Handle(ListAndCreateDashboardHandler, token.NewAuthenticationMiddleware(s.ListDashboardsHandler)).Methods(http.MethodGet)
+
+	request := httptest.NewRequest(http.MethodGet, ListAndCreateDashboardHandler, nil)
+
+	accessToken := token.NewAccessToken()
+	accessToken.SetUserInfo(models.UserInfo{
+		UserID: 1,
+	})
+
+	ctx := context.WithValue(context.Background(), token.AccessTokenContextKey, accessToken)
+	request.WithContext(ctx)
+
+	r.ServeHTTP(w, request)
+	require.Equal(t, http.StatusOK, w.Code)
+}
